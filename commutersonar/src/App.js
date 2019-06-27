af69a0a394b51ded5cac55f565452da2d691b6c1
dragosh craciun <dragosh.craciun@appliedtechnology.se>
2019-06-27 13:22:24 +0200

    parse user input

--- commutersonar/src/App.js
+++ commutersonar/src/App.js
@@ -8,13 +8,21 @@ function App() {
   const [stateFromGeolocation, setStateFromGeolocation] = useState(undefined);
   const [stateToGeolocation, setStateToGeolocation] = useState(undefined);
   const [stateTrip, setStateTrip] = useState(undefined);
+  const [stateError, setStateError] = useState(undefined);
 
   const getInput = async () => {
     const [ travelFrom, travelTo ] = document.querySelectorAll('input');
-    getGeolocation(travelFrom.value, setStateFromGeolocation);
-    getGeolocation(travelTo.value, setStateToGeolocation);
+    const trip = parseInput(travelFrom, travelTo);
+    getGeolocation(trip.origin, setStateFromGeolocation);
+    getGeolocation(trip.destination, setStateToGeolocation);
     makeSound(Date.now());
     getTime({lat: 59.8586, lon: 17.6389}, {lat: 59.3293, lon: 18.0686}, setStateTrip)
+  }
+
+  const parseInput = (originRaw, destinationRaw) => {
+    const origin = originRaw.value.trim().toLowerCase();
+    const destination = destinationRaw.value.trim().toLowerCase();
+    return { origin, destination }
   }
 
   return (
@@ -26,6 +34,7 @@ function App() {
         {stateFromGeolocation && <p>{stateFromGeolocation.lat} {stateFromGeolocation.lon}</p>}
         {stateToGeolocation && <p>{stateToGeolocation.lat} {stateToGeolocation.lon}</p>}
         {stateTrip && <p>{stateTrip}</p>}
+        {stateError && <p>{stateError}</p>}
         </header>
     </div>
   );

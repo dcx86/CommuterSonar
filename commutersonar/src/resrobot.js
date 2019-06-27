6e9f0c00f8c2b8b41d991150afb45710c51d5cc2
dragosh craciun <dragosh.craciun@appliedtechnology.se>
2019-06-27 12:56:44 +0200

    set env for hide api key

--- commutersonar/src/resrobot.js
+++ commutersonar/src/resrobot.js
@@ -5,7 +5,8 @@ export const getGeolocation = (location, setState) => {
 }
 
 export const getTime = (origin, destination, setState) => {
-  fetch(`https://api.resrobot.se/v2/trip?key=c34b3b60-fb6e-42ba-8593-1a7e611ca318&originCoordLat=${origin.lat}&originCoordLong=${origin.lon}&destCoordLat=${destination.lat}&destCoordLong=${destination.lon}&format=json`)
+  console.log(process.env.REACT_APP_RESROBOT_API_KEY)
+  fetch(`https://api.resrobot.se/v2/trip?key=${process.env.REACT_APP_RESROBOT_API_KEY}&originCoordLat=${origin.lat}&originCoordLong=${origin.lon}&destCoordLat=${destination.lat}&destCoordLong=${destination.lon}&format=json`)
   .then(res => res.json())
   .then(data => setState(data.Trip[0].LegList.Leg[0].Destination.time));
 }

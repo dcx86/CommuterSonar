e0a1f62498f2672c6635181e883b5f98c015a881
dragosh craciun <dragosh.craciun@appliedtechnology.se>
2019-06-27 09:48:08 +0200

    Colect data from input fiels

--- commutersonar/src/App.js
+++ commutersonar/src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getInput = () => {
+    const [ travelFrom, travelTo ] = document.querySelectorAll('input');
+    console.log(travelFrom.value, travelTo.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App__header">
+        <input className="App__input" type="text" placeholder="From:"></input>
+        <input className="App__input" type="text" placeholder="To:"></input>
+        <button className="App__button" onClick={getInput}>Search</button>
       </header>
     </div>
   );
